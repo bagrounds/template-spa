21c61c65c0a0fe3f0f738fcd8e18fc810e431c8c
bagrounds <bagrounds@gmail.com>
2016-06-09 16:33:59 -0700

    add test stub

---
+++ test/test-template-spa.js
@@ -0,0 +1,19 @@
+/**
+ * Tests for template-spa
+ */
+;(function () {
+  /* global describe, it */
+  'use strict'
+
+  /* Imports */
+  var expect = require('chai').expect
+
+  /* Tests */
+  describe('template-spa', function () {
+    describe('stub', function () {
+      it('should pass', function () {
+        expect(true).to.be.true
+      })
+    })
+  })
+})()
